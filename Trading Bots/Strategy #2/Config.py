8653cfac5f125d94e8f8e80721941f9ac6185b99
David Levin <72104334+davidlevinwork@users.noreply.github.com>
2022-06-10 12:58:15 +0300

    Update Config.py

--- Trading Bots/Strategy #2/Config.py
+++ Trading Bots/Strategy #2/Config.py
@@ -9,12 +9,16 @@ API_KEY = "PUBLIC KEY HERE"
 # Binance API private key
 SECRET_KEY = "PRIVATE KEY HERE"
 
+# Which fiat coins you want to use for comparing values
 FiatCoins = ['EUR', 'AUD', 'GBP']
 
+# Coins that we want to use that not exist in the basic API
 NotExsistCoins = ['BCC', 'VEN', 'PAX', 'BCHABC', 'BCHSV', 'USDSB', 'ERD', 'NPXS', 'STORM', 'HC', 'MCO', 'BULL', 'BEAR', 'ETHBULL', 'ETHBEAR', 'EOSBULL', 'EOSBEAR', 'XRPBULL', 'XRPBEAR', 'STRAT', 'BNBBULL', 'BNBBEAR', 'XZC', 'LEND', 'BKRW', 'DAI']
 
+# Crypto fiat coins
 USDCoins = ['BUSD', 'SUSD', 'USDP', 'USDC', 'TUSD', 'USDS']
 
+# Which coins we would like to ignore via the trading
 IgnoreCoins = ['ACM', 'DCR', 'REN']
 
 #######################################################
